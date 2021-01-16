6864bd17a893521780d4afaa1eb7b271c3a1c4dc
Guido Giuntoli <gagiuntoli@gmail.com>
2021-01-16 21:55:51 +0100

    Editing file names and folder

--- blockchain_programming_udemy/s4_transaction/transaction.py
+++ blockchain_programming_udemy/s4_transaction/transaction.py
@@ -1,53 +1,6 @@
 import sys
-sys.path.append("../s2_digital_signature/")
+sys.path.append("../s2_signature/")
 import signature
-
-#from cryptography.hazmat.backends import default_backend
-#from cryptography.hazmat.primitives.asymmetric import rsa
-#from cryptography.hazmat.primitives import hashes
-#from cryptography.hazmat.primitives.asymmetric import padding
-#from cryptography.exceptions import InvalidSignature
-#
-#def generate_keys():
-#
-#	private_key = rsa.generate_private_key(
-#			public_exponent=65537,
-#			key_size=2048,
-#			backend=default_backend()
-#			)
-#	public_key = private_key.public_key()
-#	return private_key, public_key
-#
-#def sign(message, private_key):
-#
-#	message = bytes(str(message), 'utf-8')
-#	signature = private_key.sign(
-#		message,
-#		padding.PSS(
-#			mgf=padding.MGF1(hashes.SHA256()),
-#			salt_length=padding.PSS.MAX_LENGTH
-#			),
-#		 hashes.SHA256()
-#	)
-#	return signature
-#
-#def verify(message, signature, public_key):
-#
-#	message = bytes(str(message), 'utf-8')
-#	try:
-#		public_key.verify(
-#     			signature,
-#     			message,
-#     			padding.PSS(
-#         			mgf=padding.MGF1(hashes.SHA256()),
-#         			salt_length=padding.PSS.MAX_LENGTH
-#     			),
-#     			hashes.SHA256()
-# 		)
-#		return True
-#
-#	except InvalidSignature:
-#		return False
 
 class Tx:
 
